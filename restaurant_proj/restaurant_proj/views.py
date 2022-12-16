ea19fb104f0a8b81f1cdd975a0bd99d243e79a0e
stanford30 <stanleyzheng84@gmail.com>
2022-12-16 17:15:34 -0600

    testing merge

--- restaurant_proj/restaurant_proj/views.py
+++ restaurant_proj/restaurant_proj/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse
 
+
 def send_the_homepage(request):
     print("home")
     theIndex = open("static/index.html").read()
     return HttpResponse(theIndex)
 
 
-    
+#  checking something really quick..
