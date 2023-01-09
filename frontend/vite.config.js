0207ec0ce9306b38f096ac8adbd838c1fef30352
stanford30 <stanleyzheng84@gmail.com>
2023-01-09 11:31:40 -0600

    updated vite config to reflect folder name change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   base: "/static/",
   build: {
     // this is the folder where vite will generate its output. Make sure django can serve files from here!
-    outDir: "../restaurant_proj/static",
+    outDir: "../backend/static",
 
     // delete the old build when creating the new build.
     // this is the default behavior, unless outDir is outside of the current directory
